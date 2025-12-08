39a6320d4b92cdd5acc6a80c3cf2bb43b13f5925
Kymylyy <kamil.marek.moson@gmail.com>
2025-12-08 18:37:10 +0100

    Update CSV data to ESMA register from 8 December 2025 and update last updated date

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -240,7 +240,7 @@ function App() {
                   >
                     available here
                   </a>
-                  {' '}• Last updated: 4 December 2025
+                  {' '}• Last updated: 8 December 2025
                 </p>
               </div>
             
