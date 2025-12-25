9082710cdc52cbd01231a4a82bc7b286c450d189
Kymylyy <kamil.marek.moson@gmail.com>
2025-12-25 12:16:06 +0100

    Update CSV data to ESMA register from 23 December 2025

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -240,7 +240,7 @@ function App() {
                   >
                     available here
                   </a>
-                  {' '}• Last updated: 15 December 2025
+                  {' '}• Last updated: 23 December 2025
                 </p>
               </div>
             
